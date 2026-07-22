679dadf4aee8544f09423f2f93d1eb33de5f6c7d
Harshavardhan Bala <harshabala025@gmail.com>
2026-07-22 17:26:19 -0500

    Got rent data across various zip codes in Dallas

--- house-price-predictor/Work.py
+++ house-price-predictor/Work.py
@@ -1,10 +1,37 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.datasets import fetch_california_housing
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-from xgboost import XGBRegressor
-model = XGBRegressor()
+from dotenv import load_dotenv
+import requests
+import os
+import json
+
+load_dotenv()
+token = os.environ["HUD_ACCESS_TOKEN"]
+headers = {"Authorization": f"Bearer {token}"}
+
+response = requests.get(
+    "https://www.huduser.gov/hudapi/public/fmr/listMetroAreas",
+    headers=headers
+)
+
+print(response.status_code)
+data = response.json()
+
+# for area in data:
+#     if("Dallas" in area["area_name"]):
+#        print(area)
+
+#use METRO19100M19100 cause that is dallas we now get all the zipcodes from that place
+
+response2 = requests.get(
+    "https://www.huduser.gov/hudapi/public/fmr/data/METRO19100M19100",
+    headers = headers   
+)
+
+data_area = response2.json()
+
+print(data_area)
+
+
+for cool in data_area['data']['basicdata']:
+    print(cool)
+
+
