6d37f5e1f7a56d77b8c3922a5f7e7706f28a45da
Harshavardhan Bala <harshabala025@gmail.com>
2026-08-06 16:40:35 -0500

    Starting working on Frontend, called the webpage Havel

---
+++ havel-backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+import json
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+model = joblib.load('rent_model.pkl')
+
+with open('model_columns.json', 'r') as f:
+    model_columns = json.load(f)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class RentRequest(BaseModel):
+    zip_code: str
+    bedrooms: int
+
+@app.get("/")
+def home():
+    return {"message": "Rent predictor API is running"}
+
+@app.post("/predict")
+def predict_rent(request: RentRequest):
+    input_data = pd.DataFrame(columns=model_columns)
+    input_data.loc[0] = 0
+
+    input_data['bedrooms'] = request.bedrooms
+    zip_column = f"zip_code_{request.zip_code}"
+
+    if zip_column in model_columns:
+        input_data[zip_column] = 1
+
+    prediction = model.predict(input_data)
+    
+    return {"predicted_rent": float(prediction[0])}
+
+    
+
